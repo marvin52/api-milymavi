336881e1c3a26897c5d063af36cddd2e05e9995e
Marvin Medeiros <marvin.medeiros52@gmail.com>
2023-11-20 20:09:44 -0300

    proxy to rss

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
-const express = require('express')
-const app = express()
-const port = 3000
-var _request = require('request');
-var _xml2js = require('xml2js').parseString;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
 
-function fetchFeed(url, cb){
-    _request({
-        url: url,
-        headers: {
-            'User-Agent': '	Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0'
-        }
-    }, function (error, response, xml){
+app.get("/", (req, res) => res.type('html').send(html));
 
-        // valid response ?
-        if (!error && response.statusCode == 200){
-            // try to parse xml feed
-            _xml2js(xml, function (xmlerror, xmldata){
-                // error ?
-                if (xmlerror){
-                    cb(xmlerror, null);
-                    return;
-                }
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-                cb(null, xmldata);
-            });
-        }else{
-            cb(error, null);
-        }
-    });
-}
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
 
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-
-fetchFeed('https://feeds.soundcloud.com/users/soundcloud:users:1325580648/sounds.rss', function(err, data){
-    if (err){
-        console.error(err);
-        return;
-    }
-
-    res.send(JSON.stringify(data));
-});
-  //res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
